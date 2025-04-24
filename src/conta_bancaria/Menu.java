02c3c0d326641bf838ea0ae36b6aeb77e82f51ea
Luiza_Gonck <luizae.goncalves@hotmail.com>
2025-04-24 12:02:41 -0300

    :sparkles: Menu

---
+++ src/conta_bancaria/Menu.java
@@ -0,0 +1,96 @@
+package conta_bancaria;
+
+import java.util.Scanner;
+
+import conta_bancaria.util.Cores;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner leia = new Scanner(System.in);
+		int opcao;
+		
+		while (true) {
+	
+		System.out.println(Cores.ANSI_BLACK_BACKGROUND+"                                                    ");
+		System.out.println(Cores.ANSI_BLACK_BACKGROUND+Cores.TEXT_CYAN_BOLD_BRIGHT+"            BANCO DO BRAZIL COM Z                   ");
+		System.out.println(Cores.ANSI_BLACK_BACKGROUND+"                                                    ");
+		System.out.println(Cores.ANSI_BLACK_BACKGROUND+Cores.TEXT_CYAN_BOLD_BRIGHT+"****************************************************");
+		System.out.println(Cores.ANSI_BLACK_BACKGROUND+"                                                    ");
+		System.out.println(Cores.ANSI_BLACK_BACKGROUND+"                                                    ");
+		System.out.println(Cores.ANSI_BLACK_BACKGROUND+Cores.TEXT_CYAN_BOLD_BRIGHT+"             1 - Criar Conta                        ");
+		System.out.println(Cores.ANSI_BLACK_BACKGROUND+Cores.TEXT_CYAN_BOLD_BRIGHT+"             2 - Listar todas as Contas             ");
+		System.out.println(Cores.ANSI_BLACK_BACKGROUND+Cores.TEXT_CYAN_BOLD_BRIGHT+"             3 - Buscar Conta por número            ");
+		System.out.println(Cores.ANSI_BLACK_BACKGROUND+Cores.TEXT_CYAN_BOLD_BRIGHT+"             4 - Atualizar Dados da Conta           ");
+		System.out.println(Cores.ANSI_BLACK_BACKGROUND+Cores.TEXT_CYAN_BOLD_BRIGHT+"             5 - Apagar Conta                       ");
+		System.out.println(Cores.ANSI_BLACK_BACKGROUND+Cores.TEXT_CYAN_BOLD_BRIGHT+"             6 - Sacar                              ");
+		System.out.println(Cores.ANSI_BLACK_BACKGROUND+Cores.TEXT_CYAN_BOLD_BRIGHT+"             7 - Depositar                          ");
+		System.out.println(Cores.ANSI_BLACK_BACKGROUND+Cores.TEXT_CYAN_BOLD_BRIGHT+"             8 - Transferir                         ");
+		System.out.println(Cores.ANSI_BLACK_BACKGROUND+Cores.TEXT_CYAN_BOLD_BRIGHT+"             9 - Sair                               ");
+		System.out.println(Cores.ANSI_BLACK_BACKGROUND+"                                                    ");
+		System.out.println(Cores.ANSI_BLACK_BACKGROUND+Cores.TEXT_CYAN_BOLD_BRIGHT+"****************************************************");
+		System.out.println(Cores.ANSI_BLACK_BACKGROUND+"                                                    ");
+		System.out.println(Cores.ANSI_BLACK_BACKGROUND+"                                                    ");
+		
+		opcao = leia.nextInt();
+
+		if (opcao == 9) {
+			System.out.println("\nBanco do Brazil com Z - O seu Futuro começa aqui!");
+			sobre();
+             leia.close();
+			System.exit(0);
+		}
+
+		switch (opcao) {
+			case 1:
+				System.out.println("Criar Conta\n\n");
+
+				break;
+			case 2:
+				System.out.println("Listar todas as Contas\n\n");
+
+				break;
+			case 3:
+				System.out.println("Consultar dados da Conta - por número\n\n");
+
+				break;
+			case 4:
+				System.out.println("Atualizar dados da Conta\n\n");
+
+				break;
+			case 5:
+				System.out.println("Apagar a Conta\n\n");
+
+				break;
+			case 6:
+				System.out.println("Saque\n\n");
+
+				break;
+			case 7:
+				System.out.println("Depósito\n\n");
+
+				break;
+			case 8:
+				System.out.println("Transferência entre Contas\n\n");
+
+				break;
+			default:
+				System.out.println("\nOpção Inválida!\n");
+				break;
+		  }
+				
+		}
+	}
+		
+		public static void sobre() {
+			System.out.println("\n*********************************************************");
+			System.out.println("Projeto Desenvolvido por: ");
+			System.out.println("Generation Brasil - generation@generation.org");
+			System.out.println("github.com/conteudoGeneration");
+			System.out.println("*********************************************************");
+		
+	}
+
+}
